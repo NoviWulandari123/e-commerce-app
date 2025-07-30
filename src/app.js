80e64525c2ba2ae0a698a1b199195c408bf3b515
NoviWulandari123 <noviwulandari1104@gmail.com>
2025-07-30 13:27:37 +0800

    feat:create products API

--- src/app.js
+++ src/app.js
@@ -1,18 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+
+// Import routes
+const productRoutes = require('./routes/productRoutes');
+
+// Middleware
 app.use(express.json()); // Middleware untuk parsing JSON body
 
-
-// Rute untuk setiap modul
-
+// Routes
+app.use('/api/products', productRoutes);
 
 // Rute dasar
 app.get('/', (req, res) => {
    res.send('Welcome to the E-commerce API!');
 });
 
-
+// Jalankan server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
    console.log(`Server running on port ${PORT}`);
